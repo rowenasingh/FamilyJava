3cb99e489449054c1c143018ea0e634b54a68c0a
khughes <hughes.khalil@gmail.com>
2017-04-25 15:27:45 +0100

    Added some methods and constructors

--- Family.java
+++ Family.java
@@ -14,19 +14,43 @@
 
 
 class Parent {
-	String sex;
-	String eyeColor;
-	String hairColor;
-	String hairTexture;
-	double height;
+	private String sex;
+	private String eyeColor;
+	private String hairColor;
+	private String hairTexture;
+	private double height;
 	double penisSize;
 	private boolean creditCard;
 
+	String getSex() {
+		return sex;
+	}
+
+	String getEyeColor() {
+		return eyeColor;
+	}
+
+	String getHairColor() {
+		return hairColor;
+	}
+
+	String getHairTexture() {
+		return hairTexture;
+	}
+
+	double getHeight() {
+		return height;
+	}
+
 	Parent() {
+		sex = "Not defined";
 		eyeColor = "Not defined";
 		hairColor = "Not defined";
 		hairTexture = "Not defined";
 		height = -1;
+		penisSize = -1;
+		creditCard = false;
+		showAll();
 	} 
 
 	Parent(String a, String b, String c, String d, double e, double f, boolean g) {
@@ -40,13 +64,49 @@ class Parent {
 		if (sex == "male") {
 			penisSize = f;
 		}
-		
+
+		if (sex == "female") {
+			penisSize = -1;
+		}
+
 		creditCard = g;
 
-
+		showAll();
 	}
+
+	void showAll() {
+		System.out.println(getSex());
+		System.out.println(getEyeColor());
+		System.out.println(getHairColor());
+		System.out.println(getHairTexture());
+		System.out.println(getHeight());
+		System.out.println();
+	}
+
 }
 
 class Child  extends Parent{
 
+	Child() {
+		super();
+	}
+
+	Child(Parent ob0, Parent ob1) {
+
+	}
+
+}
+
+
+class Family {
+	public static void main(String[] args) {
+		
+		Parent father = new Parent("male", "brown", "blond", "kinky", 69.5, 5.5, true);
+
+		Parent mother = new Parent("female", "blue", "red", "straight", 64.0, 0, false);
+
+		System.out.println(mother.penisSize);
+
+		Child son = new Child();
+	}
 }
